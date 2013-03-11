fc8828064f32a3cf8924ae162cc5ba10dba71c18
prathyusha <prathyusha@cloudboxonline.com>
2013-03-11 14:23:50 +0530

    PHP files

---
+++ calculator.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Calculator Application";
+
+?> 
+
+<br>
+ The sum of two numbers <?php echo $_GET["num1"]; ?> and <?php echo $_GET["num2"]; ?> is  <br>
+<?php echo $_GET["num1"]+$_GET["num2"]; ?> !
