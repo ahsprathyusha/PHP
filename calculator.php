392ab97f3e4688ea1bf5c1c4ffb3dbd395866eba
prathyusha <prathyusha@cloudboxonline.com>
2013-03-11 14:30:23 +0530

    test github

--- calculator.php
+++ calculator.php
@@ -6,4 +6,4 @@ echo "Calculator Application";
 
 <br>
  The sum of two numbers <?php echo $_GET["num1"]; ?> and <?php echo $_GET["num2"]; ?> is  <br>
-<?php echo $_GET["num1"]+$_GET["num2"]; ?> !
+<?php echo $_GET["num1"]+$_GET["num2"]; ?> .
